489a69852ec3d7fcf32d6d19f2e2504fa246eef6
Sharinogan <voragun2543@gmail.com>
2022-09-27 17:04:47 +0700

    count instance

--- code/main/Start.java
+++ code/main/Start.java
@@ -10,10 +10,46 @@ import org.springframework.web.bind.annotation.RestController;
 
 class Start {
     public static void main(String[] args) {
-        ApplicationContext context;
-        context = SpringApplication.run(Setup.class);
+        // ApplicationContext context;
+        // context = SpringApplication.run(Setup.class);
+        
+        Car c = new Car();
+        System.out.println( c instanceof Car );     // true
+        System.out.println( c instanceof Vehicle ); // true
+        System.out.println( c instanceof Object );  // true
+        
+        
+        Vehicle[] a = { new Car(), new Truck(), new Car(),
+                        new Truck(), new Car(), new Car() };
+        int count= 0;
+        for (int i = 0; i < a.length; i++) {
+            if(a[i] instanceof Car) {
+                count++;
+            }
+        }
+        System.out.println(count);
     }
 }
+
+class Vehicle {  }
+
+class Car extends Vehicle {  }
+
+class Truck extends Vehicle {  }
+
+class Ship extends Vehicle {  }
+
+class Product {
+    double price;
+    double getSpecialPrice() {
+        return 0.90 * price;
+    }
+}
+
+class Book extends Product {  }
+//     '               '--------------> Super class
+//     '------------------------------> Sub class
+
 
 @SpringBootApplication
 class Setup {
